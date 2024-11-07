47f34a730cdc76be9caa6c012f616bcfbda301fe
Grabner Mathias <grabne200054@sr.htlweiz.at>
2024-11-07 15:44:24 +0100

    restructured customer signup

--- src/DataAnalysis/descriptive/CustomerSignup.py
+++ src/DataAnalysis/descriptive/CustomerSignup.py
@@ -2,6 +2,7 @@ from DataAnalysis.descriptive.DescriptiveAnalysis import DescriptiveAnalysis
 from DataAnalysis.APIDataHandlerFactory import APIDataHandlerFactory
 
 from datetime import datetime, timedelta
+from collections import defaultdict
 
 
 class CustomerSignup(DescriptiveAnalysis):
@@ -19,36 +20,83 @@ class CustomerSignup(DescriptiveAnalysis):
         """
         return self.handler.start()
         
-    def perform(self, last_days: int = 0) -> tuple:
+    def perform(self, last_days: int = 0, year: bool = False, month: bool = False) -> dict:
         """
         Perform the analysis
 
         Args:
             last_days (int, optional): Number of days to consider. Defaults to 0.
+            year (bool, optional): If True, returns the yearly growth. Defaults to False.
+            month (bool, optional): If True, returns the monthly growth. Defaults to False.
 
         Returns:
-            tuple: Tuple containing the customers and dates
+            dict: Dictionary containing growth as dictionary and cumulative growth data as dictionary
         """
         data = self.collect()
 
-        customers = []
-        dates = []
+        data.sort(key=lambda i: datetime.strptime(i['signedUp'], "%Y-%m-%dT%H:%M:%S.%f")) # Sort by signedUp date
+
+        if year:
+            return self._setYearlyGrowth(data)
         
-        data.sort(key=lambda i: datetime.strptime(i['signedUp'], "%Y-%m-%dT%H:%M:%S.%f")) # Sort by signedUp date)
+        elif month:
+            return self._setMonthlyGrowth(data)
         
-        for i in data:
-            should_append = True
-            if last_days > 0:
-                if datetime.strptime(i['signedUp'], "%Y-%m-%dT%H:%M:%S.%f") <= datetime.now() - timedelta(days=last_days):
-                    should_append = False  # Don't append if date is older than last_days
-
-            if should_append:
-                i['signedUp'] = i['signedUp'].split("t")[0]
-                customers.append(i['lastname'] + ", " + i['firstname'])
-                dates.append(i['signedUp'])
-        return (customers, dates)
-
-
+        else:
+            return self._setGrowthByDays(data, last_days)
+        
 
     def report(self):
         pass
+
+    def _setYearlyGrowth(self, data: list) -> dict:
+        yearlygrowth = defaultdict(int)
+        cumulative_growth = {}
+        total = 0 
+
+        for i in data:
+            year = i['signedUp'].split("-")[0]
+            
+            yearlygrowth[year] += 1
+            total += 1
+            
+            cumulative_growth[year] = total
+
+        return {"growth": dict(yearlygrowth), "cumulative_growth": cumulative_growth}
+    
+    def _setMonthlyGrowth(self, data: list) -> dict:
+        monthlygrowth = defaultdict(int)
+        cumulative_growth = {}
+        total = 0
+
+        for i in data:
+            if datetime.strptime(i['signedUp'], "%Y-%m-%dT%H:%M:%S.%f").year == datetime.now().year:
+                month = i['signedUp'].split("-")[1]
+                monthlygrowth[month] += 1
+                total += 1
+
+                cumulative_growth[month] = total
+        
+        return {"growth": dict(monthlygrowth), "cumulative_growth": cumulative_growth}
+    
+    def _setGrowthByDays(self, data: list, last_days: int) -> dict:
+        
+        growth = defaultdict(int)
+        total = 0
+
+        cumulative_growth = {}
+
+        for i in data:
+            i['signedUp'] = i['signedUp'].split("t")[0]
+            if last_days > 0:
+                if datetime.strptime(i['signedUp'], "%Y-%m-%dT%H:%M:%S.%f") >= datetime.now() - timedelta(days=last_days):
+                    growth[i['signedUp']] += 1
+                    total += 1
+
+                    cumulative_growth[i['signedUp']] = total
+            else:
+                growth[i['signedUp']] += 1
+                total += 1
+                cumulative_growth[i['signedUp']] = total
+        
+        return {"growth": dict(growth), "cumulative_growth": cumulative_growth}
