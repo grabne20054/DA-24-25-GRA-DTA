a6b6c9ecf1562b7e39940b66c43d35e461eae3bf
Grabner Mathias <grabne200054@sr.htlweiz.at>
2025-02-10 21:58:25 +0100

    refactor

--- src/API/crud/crud.py
+++ src/API/crud/crud.py
@@ -4,19 +4,18 @@ from DataAnalysis.descriptive import CustomerSignup, EmployeeAmount, ProductsAmo
 
 from DataAnalysis.diagnostic import ProductOrdersCorrelation, ItemBoughtCorrelation
 
+from DataAnalysis.predictive.PredictiveEngine.DataPredictor import DataPredictor
+
 from api.auth import generate_jwt_token
 
 from dotenv import load_dotenv
 from os import getenv
-import bcrypt
-
 import requests
+from datetime import datetime, timedelta
 load_dotenv()
 
 
-async def get_customers_signup(last_days: int = None, month: bool = False, year: bool = False):
-    if last_days is None:
-        last_days = 0
+async def get_customers_signup(last_days: int = 0, month: bool = False, year: bool = False):
     return CustomerSignup.CustomerSignup().perform(last_days=last_days, month=month, year=year)
 
 async def get_employees_amount():
@@ -25,13 +24,11 @@ async def get_employees_amount():
 async def get_products_amount():
     return ProductsAmount.ProductsAmount().perform()
 
-async def get_products_mostly_bought(last_days: int = None, month: bool = False, year: bool = False):
-    if last_days is None:
-        last_days = 0
+async def get_products_mostly_bought(last_days: int = 0, month: bool = False, year: bool = False):
     return ProductsMostlyBought.ProductsMostlyBought().perform(last_days=last_days, month=month, year=year)
 
-async def get_routes_amount():
-    return RoutesAmount.RoutesAmount().perform()
+async def get_routes_amount(n_amount: int = 0):
+    return RoutesAmount.RoutesAmount().perform(n_amount=n_amount)
 
 async def get_products_orders_correlation():
     if ProductOrdersCorrelation.ProductOrdersCorrelation().perform() is None:
@@ -42,10 +39,21 @@ async def get_changing_price_orders_correlation(price_percentage: float = 0.1, n
     return ProductOrdersCorrelation.ProductOrdersCorrelation().getChangingPriceOrdersCorrValue(price_percentage=price_percentage, n_random=n_random)
 
 async def get_items_bought_correlation(product_name: str, amount_combined_products: int):
-    raise ItemBoughtCorrelation.ItemBoughtCorrelation().perform(product_name=product_name, amount_combined_products=amount_combined_products)
+    return ItemBoughtCorrelation.ItemBoughtCorrelation().perform(product_name=product_name, combination_product_amount=amount_combined_products)
 
-async def get_customers_growth(X_data: list):
-    raise HTTPException(status_code=501, detail="Not implemented")
+async def get_customers_growth(last_days: int = None, month: bool = False, year: bool = False):
+    if year is True:
+        X_data = datetime.now().year
+    elif month is True:
+        X_data = [i for i in range(1, 13)]
+    elif last_days is not None:
+        X_data = [datetime.now() - timedelta(days=i) for i in range(last_days)]
+        print(X_data)
+    else: 
+        raise HTTPException(status_code=400, detail="Invalid parameters")
+   
+    return DataPredictor.DataPredictor("CustomerGrowth").predict(X_data, "CustomerGrowth")
+
 
 async def get_cumulative_customers_growth():
     raise HTTPException(status_code=501, detail="Not implemented")
