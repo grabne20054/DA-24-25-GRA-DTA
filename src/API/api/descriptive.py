6a38d70e4f270369462091df9ae4a8afb4ace0c7
Grabner Mathias <grabne200054@sr.htlweiz.at>
2025-02-10 21:55:36 +0100

    add Exception Handling

--- src/API/api/descriptive.py
+++ src/API/api/descriptive.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from typing import Annotated
 
 
@@ -11,27 +11,50 @@ from api.auth import is_token_valid
 router = APIRouter()
 
 @router.get(f"/{VERSION}/{DESCRIPTIVE}/customers-signup/", status_code=210)
-async def get_customers_signup(token: Annotated[str, Depends(is_token_valid)], last_days: int = None, month: bool = False, year: bool = False):
-    data = await crud.get_customers_signup(last_days=last_days, month=month, year=year)
-    return data
-    
+async def get_customers_signup(token: Annotated[str, Depends(is_token_valid)], last_days: int = 0, month: bool = False, year: bool = False):
+    try:
+        data = await crud.get_customers_signup(last_days=last_days, month=month, year=year)
+        return data
+    except Exception as e:
+        if e == "No data found":
+            raise HTTPException(status_code=404, detail=str(e))
+        else:
+            raise HTTPException(status_code=400, detail=str(e))
 
 @router.get(f"/{VERSION}/{DESCRIPTIVE}/employees-amount/", status_code=210)
 async def get_employees_amount(token: Annotated[str, Depends(is_token_valid)]):
-    data = await crud.get_employees_amount()
-    return data
-
+    try:
+        data = await crud.get_employees_amount()
+        return data
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
+    
+# TODO: LIMIT
 @router.get(f"/{VERSION}/{DESCRIPTIVE}/products-amount/", status_code=210)
 async def get_products_amount(token: Annotated[str, Depends(is_token_valid)]):
-    data = await crud.get_products_amount()
-    return data
-
+    try:
+        data = await crud.get_products_amount()
+        return data
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
+    
+# TODO: orderDate, LIMIT
 @router.get(f"/{VERSION}/{DESCRIPTIVE}/products-mostly-bought/", status_code=210)
-async def get_products_mostly_bought(token: Annotated[str, Depends(is_token_valid)]):
-    data = await crud.get_products_mostly_bought()
-    return data
+async def get_products_mostly_bought(token: Annotated[str, Depends(is_token_valid)], last_days: int = None, month: bool = False, year: bool = False):
+    try:
+        data = await crud.get_products_mostly_bought(last_days=last_days, month=month, year=year)
+        return data
+    except Exception as e:
+        if e == "No data found":
+            raise HTTPException(status_code=404, detail=str(e))
+        else:
+            raise HTTPException(status_code=400, detail=str(e))
 
 @router.get(f"/{VERSION}/{DESCRIPTIVE}/routes-amount/", status_code=210)
-async def get_routes_amount(token: Annotated[str, Depends(is_token_valid)]):
-    data = await crud.get_routes_amount()
-    return data
+async def get_routes_amount(token: Annotated[str, Depends(is_token_valid)], n_amount: int = 5):
+    try:
+        data = await crud.get_routes_amount(n_amount=n_amount)
+        return data
+    except Exception as e:        
+        raise HTTPException(status_code=404, detail=str(e))
+    
--- src/API/api/diagnostic.py
+++ src/API/api/diagnostic.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from typing import Annotated
 
 from crud import crud
@@ -10,15 +10,27 @@ router = APIRouter()
 
 @router.get(f"/{VERSION}/{DIAGNOSTIC}/products-orders-correlation/", status_code=210)
 async def get_products_orders_correlation(token: Annotated[str, Depends(is_token_valid)]):
-    data = await crud.get_products_orders_correlation()
+    try:
+        data = await crud.get_products_orders_correlation()
+
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))    
     return data
 
 @router.get(f"/{VERSION}/{DIAGNOSTIC}/products-orders-correlation/change-price", status_code=210)
 async def get_changing_price_orders_correlation(token: Annotated[str, Depends(is_token_valid)], price_percentage: float = 0.1, n_random: int = 0):
-    data = await crud.get_changing_price_orders_correlation(price_percentage=price_percentage, n_random=n_random)
+    try:
+        data = await crud.get_changing_price_orders_correlation(price_percentage=price_percentage, n_random=n_random)
+
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
     return data
 
 @router.get(f"/{VERSION}/{DIAGNOSTIC}/items-bought-correlation/", status_code=210)
 async def get_items_bought_correlation(token: Annotated[str, Depends(is_token_valid)], product_name: str, amount_combined_products: int):
-    data = await crud.get_items_bought_correlation(product_name, amount_combined_products)
+    try:
+
+        data = await crud.get_items_bought_correlation(product_name, amount_combined_products)
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
     return data
