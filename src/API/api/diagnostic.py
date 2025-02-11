6470bb2f7ed71266bb82b8b4cb8035c4632a8eae
Grabner Mathias <grabne200054@sr.htlweiz.at>
2025-02-11 16:29:49 +0100

    fixed Bug, add Exception Handling

--- src/API/api/diagnostic.py
+++ src/API/api/diagnostic.py
@@ -12,25 +12,25 @@ router = APIRouter()
 async def get_products_orders_correlation(token: Annotated[str, Depends(is_token_valid)]):
     try:
         data = await crud.get_products_orders_correlation()
-
+        return data
     except Exception as e:
-        raise HTTPException(status_code=404, detail=str(e))    
-    return data
+        raise HTTPException(status_code=400, detail=str(e))    
+
 
 @router.get(f"/{VERSION}/{DIAGNOSTIC}/products-orders-correlation/change-price", status_code=210)
 async def get_changing_price_orders_correlation(token: Annotated[str, Depends(is_token_valid)], price_percentage: float = 0.1, n_random: int = 0):
     try:
         data = await crud.get_changing_price_orders_correlation(price_percentage=price_percentage, n_random=n_random)
-
+        return data
     except Exception as e:
         raise HTTPException(status_code=404, detail=str(e))
-    return data
+    
 
 @router.get(f"/{VERSION}/{DIAGNOSTIC}/items-bought-correlation/", status_code=210)
 async def get_items_bought_correlation(token: Annotated[str, Depends(is_token_valid)], product_name: str, amount_combined_products: int):
     try:
-
         data = await crud.get_items_bought_correlation(product_name, amount_combined_products)
+        return data
     except Exception as e:
         raise HTTPException(status_code=404, detail=str(e))
-    return data
+    
--- src/DataAnalysis/diagnostic/ProductOrdersCorrelation.py
+++ src/DataAnalysis/diagnostic/ProductOrdersCorrelation.py
@@ -9,6 +9,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+LOWER_BOUND = -1
+UPPER_BOUND = 1
+TYPEOFGRAPH = "gauge"
+
 
 class ProductOrdersCorrelation(DiagnosticAnalysis):
     """ Class for analyzing the correlation between product orders"""
@@ -37,31 +41,31 @@ class ProductOrdersCorrelation(DiagnosticAnalysis):
             print("Error: ", e)
             return None, None, None
 
-        # convert to dataframes
         df_orders = pd.DataFrame(orders)
         df_ordersProducts = pd.DataFrame(ordersProducts)
         df_products = pd.DataFrame(products)
         df_customers = pd.DataFrame(customers)
 
         return df_orders, df_ordersProducts, df_products, df_customers
-
-    def perform(self):
+    
+    def merge(self) -> pd.DataFrame:
         """
-        Perform the analysis
-        
+        Merge the dataframes
+
         Returns:
-            tuple: Tuple of correlation values between productAmount and price, orderDate, businessSector in format (price, order date, business sector)
+            pd.DataFrame: Merged dataframe
         
         Raises:
             Exception: No data found
+            Exception: One or more dataframes are empty
         """
+
         df_orders, df_ordersProducts, df_products, df_customers = self.collect()
         if df_orders is None or df_ordersProducts is None or df_products is None or df_customers is None:
             raise Exception("No data found")
         elif df_orders.empty or df_ordersProducts.empty or df_products.empty or df_customers.empty:
             raise Exception("One or more dataframes are empty")
 
-        # Merge the dataframes like a SQL join
         try:
             df_ordersProducts = pd.merge(df_ordersProducts, df_orders, on='orderId')
             df_ordersProducts = pd.merge(df_ordersProducts, df_products, on='productId')
@@ -79,36 +83,47 @@ class ProductOrdersCorrelation(DiagnosticAnalysis):
             'companyNumber', 'deleted_x', 'deleted_y', 'deleted'
         ]
 
-        # Drop columns only if they are present in the DataFrame
         df_ordersProducts = df_ordersProducts.drop(columns=df_ordersProducts.columns.intersection(columns_to_drop))
 
         
-        # date to month
         df_ordersProducts['orderDate'] = pd.to_datetime(df_ordersProducts['orderDate'])
         df_ordersProducts['orderDate'] = df_ordersProducts['orderDate'].dt.strftime('%m').astype('int64')
 
-        # Label Encoding
         labelencoder = LabelEncoder()
         df_ordersProducts['businessSector'] = labelencoder.fit_transform(df_ordersProducts['businessSector'])
 
         
         self.df_ordersProducts = df_ordersProducts
-        # Correlation
-        price_correlation = df_ordersProducts[['productAmount','price']].corr('pearson')
-        date_correlation = df_ordersProducts[['productAmount','orderDate']].corr('spearman')
-        user_correlation = df_ordersProducts[['productAmount','businessSector']].corr('spearman')
 
-        print(price_correlation)
-        print(date_correlation)
-        print(user_correlation)
 
-        return price_correlation.loc['productAmount', 'price'], date_correlation.loc['productAmount', 'orderDate'], user_correlation.loc['productAmount', 'businessSector'] # get certain value from the correlation matrix
+    def perform(self) -> dict:
+        """
+        Perform the analysis
+        
+        Returns:
+            dict: Dictionary containing the correlation values
+        Raises:
+            Exception: No data found
+            Exception: Correlation values are NaN
+        """
+        
+        self.merge()
+        if self.df_ordersProducts is None:
+            raise Exception("No data found")
+        price_correlation = self.df_ordersProducts[['productAmount','price']].corr('pearson')
+        date_correlation = self.df_ordersProducts[['productAmount','orderDate']].corr('spearman')
+        user_correlation = self.df_ordersProducts[['productAmount','businessSector']].corr('spearman')
+
+        if price_correlation.isnull().values.any() or date_correlation.isnull().values.any() or user_correlation.isnull().values.any():
+            raise Exception("Correlation values are NaN")
+       
+        return {"product-price-corr": price_correlation.loc['productAmount', 'price'], "product-orderDate-corr" : date_correlation.loc['productAmount', 'orderDate'], "product-businessSector-corr": user_correlation.loc['productAmount', 'businessSector'], "lower-bound": LOWER_BOUND, "upper-bound": UPPER_BOUND, "typeofgraph": TYPEOFGRAPH}
 
 
     def report(self):
         pass
 
-    def getChangingPriceOrdersCorrValue(self, price_percentage: float = 1, n_random: int = 0 ) -> float:
+    def getChangingPriceOrdersCorrValue(self, price_percentage: float = 1, n_random: int = 0 ) -> dict:
         """
         Get the correlation value between productAmount and price if a imaginary percentage of price change is made
 
@@ -118,17 +133,27 @@ class ProductOrdersCorrelation(DiagnosticAnalysis):
                             If 0, all prices are scaled uniformly.
                             If n > 0, n random prices are scaled by the price percentage.
 
-        Returns:
-            float: Correlation value between productAmount and price
-        """
-        copy_df = self.df_ordersProducts.copy()
+        Example:
 
+        getChangingPriceOrdersCorrValue(0.1, 0) will return the correlation value between productAmount and price if all prices are decreased by 10%
+        getChangingPriceOrdersCorrValue(1.1, 5) will return the correlation value between productAmount and price if 5 random prices are increased by 10%
+
+        Returns:
+            dict: Dictionary containing the correlation value
+        """
+        self.merge()
+        if self.df_ordersProducts is None:
+            raise Exception("No data found")
+        
+        copy_df = self.df_ordersProducts.copy()
         if n_random == 0:
             copy_df['price'] = copy_df['price'] * price_percentage
         elif n_random > 0:
+            if n_random > len(copy_df):
+                raise Exception("Number of random products to adjust is greater than the actual number of products present")
             random_products = copy_df.sample(n=n_random)
-            random_products['price'] = random_products['price'] * price_percentage
+            random_products['price'] = (random_products['price'] * price_percentage).astype(copy_df['price'].dtype)
             copy_df.update(random_products)
         
         price_correlation = copy_df[['productAmount','price']].corr('pearson')	
-        return price_correlation.loc['productAmount', 'price']
+        return {"changed-product-price-corr" : price_correlation.loc['productAmount', 'price'], "lower-bound": LOWER_BOUND, "upper-bound": UPPER_BOUND, "typeofgraph": TYPEOFGRAPH}
