c83a286dbb14147d205342e828cb8856c412c3d0
Grabner Mathias <grabne200054@sr.htlweiz.at>
2025-02-16 15:07:35 +0100

    changed status code

--- src/API/api/diagnostic.py
+++ src/API/api/diagnostic.py
@@ -8,7 +8,7 @@ from api.auth import is_token_valid
 
 router = APIRouter()
 
-@router.get(f"/{VERSION}/{DIAGNOSTIC}/products-orders-correlation/", status_code=210)
+@router.get(f"/{VERSION}/{DIAGNOSTIC}/products-orders-correlation/", status_code=211)
 async def get_products_orders_correlation(token: Annotated[str, Depends(is_token_valid)]):
     try:
         data = await crud.get_products_orders_correlation()
@@ -17,7 +17,7 @@ async def get_products_orders_correlation(token: Annotated[str, Depends(is_token
         raise HTTPException(status_code=400, detail=str(e))    
 
 
-@router.get(f"/{VERSION}/{DIAGNOSTIC}/products-orders-correlation/change-price", status_code=210)
+@router.get(f"/{VERSION}/{DIAGNOSTIC}/products-orders-correlation/change-price", status_code=211)
 async def get_changing_price_orders_correlation(token: Annotated[str, Depends(is_token_valid)], price_percentage: float = 0.1, n_random: int = 0):
     try:
         data = await crud.get_changing_price_orders_correlation(price_percentage=price_percentage, n_random=n_random)
@@ -26,7 +26,7 @@ async def get_changing_price_orders_correlation(token: Annotated[str, Depends(is
         raise HTTPException(status_code=404, detail=str(e))
     
 
-@router.get(f"/{VERSION}/{DIAGNOSTIC}/items-bought-correlation/", status_code=210)
+@router.get(f"/{VERSION}/{DIAGNOSTIC}/items-bought-correlation/", status_code=211)
 async def get_items_bought_correlation(productId: str, amount_combined_products: int):
     try:
         data = await crud.get_items_bought_correlation(productId, amount_combined_products)
