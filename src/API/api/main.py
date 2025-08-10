8742602143feb974afdff10f94569ea7f342daf8
grabne20054 <71632892+grabne20054@users.noreply.github.com>
2025-08-10 15:01:15 +0200

    Update main.py

--- src/API/api/main.py
+++ src/API/api/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from api import descriptive, diagnostic, predictive, auth
+import descriptive, diagnostic, predictive, auth
 
 
 
