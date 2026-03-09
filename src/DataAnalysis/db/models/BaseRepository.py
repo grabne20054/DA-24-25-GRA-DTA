9c24b15279cc3a3b7303e37dd7726b76e8568fae
grabne20054 <grabne20054@gmail.com>
2026-03-09 18:32:11 +0100

    Add BaseRepository and CustomerSignupRepository classes; refactor CustomerSignup to use repository pattern and update data handling

---
+++ src/DataAnalysis/db/models/BaseRepository.py
@@ -0,0 +1,13 @@
+from typing import Type, TypeVar, Generic, List
+from sqlalchemy.orm import Session
+
+T = TypeVar("T")
+
+class BaseRepository(Generic[T]):
+    def __init__(self, model: Type[T], session: Session):
+        self.model = model
+        self.session = session
+
+    def filter(self, **filters) -> List[T]:
+        data = self.session.query(self.model).filter_by(**filters).all()
+        return data
